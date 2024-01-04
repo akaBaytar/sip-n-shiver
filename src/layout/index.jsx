12fe75ba2a9b0034881b95b9672e86a580f88398
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-04 15:57:46 +0300

    navbar created and styled

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,7 +1,15 @@
+import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Navbar from '../components/Navbar';
+
 const Layout = () => {
-  return <Outlet />;
+  return (
+    <Fragment>
+      <Navbar />
+      <Outlet />
+    </Fragment>
+  );
 };
 
 export default Layout;
