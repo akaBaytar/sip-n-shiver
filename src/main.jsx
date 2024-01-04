abbef56b193880a291091656f7535d51691b6661
Burak Bilgili <burak.bilgili@icloud.com>
2024-01-04 22:20:58 +0300

    newsletter page and toast notification setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { ToastContainer } from 'react-toastify';
+
 import '../style/index.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ToastContainer position='top-right' autoClose={2000} />
   </React.StrictMode>
 );
--- src/pages/Newsletter.jsx
+++ src/pages/Newsletter.jsx
@@ -1,7 +1,77 @@
-const Newsletter = () => {
-  return (
-    <div>Newsletter</div>
-  )
-}
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
-export default Newsletter
+const Newsletter = () => {
+  const [firstname, setFirstame] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+
+  const submitHandler = () => {
+    try {
+      setFirstame('');
+      setLastname('');
+      setEmail('');
+      toast.success('Your request has been successfully received.');
+    } catch (error) {
+      toast.error('An error occurred.');
+    }
+  };
+
+  return (
+    <Form className='form' onSubmit={submitHandler}>
+      <h4 style={{ textAlign: 'center', marginBottom: '2rem' }}>Newsletter</h4>
+      <div className='form-row'>
+        <label htmlFor='first-name' className='form-label'>
+          First Name:
+        </label>
+        <input
+          type='text'
+          value={firstname}
+          onChange={(e) => setFirstame(e.target.value)}
+          name='first-name'
+          id='first-name'
+          className='form-input'
+          autoComplete='given-name'
+          required
+        />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='last-name' className='form-label'>
+          Last Name:
+        </label>
+        <input
+          type='text'
+          value={lastname}
+          onChange={(e) => setLastname(e.target.value)}
+          name='last-name'
+          id='last-name'
+          className='form-input'
+          autoComplete='family-name'
+          required
+        />
+      </div>
+      <div className='form-row'>
+        <label htmlFor='email' className='form-label'>
+          E-mail:
+        </label>
+        <input
+          type='email'
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          name='email'
+          id='email'
+          className='form-input'
+          autoComplete='email'
+          required
+        />
+      </div>
+      <span style={{ fontSize: '0.7rem', color: 'red' }}>All fields must be filled out.</span>
+      <button type='submit' className='button button-block' style={{ marginTop: '0.45rem' }}>
+        Submit
+      </button>
+    </Form>
+  );
+};
+
+export default Newsletter;
